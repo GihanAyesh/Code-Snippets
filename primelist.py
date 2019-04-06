923a1176857419c45243467529f612586f40c1cb
Gihan Ayeshmantha <gihanayeshmanthadrck@gmail.com>
2019-04-06 16:32:29 +0530

    Update primelist.py
    Best way is Sieve way

--- primelist.py
+++ primelist.py
@@ -1,18 +1,15 @@
-def getPrime(n):
-    numPrime=0
-    primeArray=[0]
-    for i in range (2,n+1):
-        sts=True
-        for m in range (n):
-            if ((primeArray[m] == 0) or (primeArray[m])>int(math.sqrt(i))):
-                break
-            elif(i%primeArray[m]==0):
-                sts=False
-                break
-        if (sts):
-            if primeArray[0]==0:
-                primeArray[0]=i
-            else:
-                primeArray+=[i]
-        numPrime+=1
-    return primeArray
+def primeList(n):
+    lis=[]
+    prime = [True for i in range(n+1)]
+    p = 2
+    while (p * p <= n):
+        if (prime[p] == True): 
+            for i in range(p * p, n+1, p): 
+                prime[i] = False
+        p += 1
+    for p in range(2, n): 
+        if prime[p]: 
+            lis+=[p]
+    return lis
+    
+
